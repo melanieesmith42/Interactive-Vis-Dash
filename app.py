3a55d3d0a656b21a5ced97464be5ef6ffdc2c9bc
Mel Smith <35923440+melanieesmith42@users.noreply.github.com>
2018-07-17 16:46:18 -0400

    yes, I have figured out the slide toggle

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.layout = html.Div(children=[
     html.H1(children='Test Data Viz'),
     
     html.Div(children='''
-        This is a Dash mockup of the data visualizations. It pulls data I made up from a .csv to represent what you might want. I have not yet figured out how I might incorporate a toggle menu, but that's coming next. There are a few ways to use Dash. I personally prefer using JupyterLab to create this because JupyterLab allows me to have a terminal open next to my app, so I can edit the .py and run in the terminal separately. This could be done in an .ipynb, but it would not have the benefit of running an active terminal, which reloads the webapp when it detects changes to the .py
+        This is a Dash mockup of the data visualizations. It pulls data I made up from a .csv to represent what you might want. There are a few ways to use Dash. I personally prefer using JupyterLab to create this because JupyterLab allows me to have a terminal open next to my app, so I can edit the .py and run in the terminal separately. This could be done in an .ipynb, but it would not have the benefit of running an active terminal, which reloads the webapp when it detects changes to the .py
     '''),
     
     dcc.Slider(
